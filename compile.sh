7848ae1958a0f4ee7a4d691d606324766842c04e
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2022-11-30 12:50:27 +0000

    Add files via upload

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
 clang main.c glad_gl.c -I inc -Ofast -lglfw -lm -o menger
+clang main_trails.c glad_gl.c -I inc -Ofast -lglfw -lm -o menger_trails
 ./menger
--- release.sh
+++ release.sh
@@ -1,2 +1,4 @@
 clang main.c glad_gl.c -I inc -Ofast -lglfw -lm -o menger
+clang main_trails.c glad_gl.c -I inc -Ofast -lglfw -lm -o menger_trails
 upx menger
+upx menger_trails
